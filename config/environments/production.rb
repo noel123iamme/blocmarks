f332ee15c8f33f7d5ffee10dbca24ac942d1232e
Noel Deguzman <noel.deguzman@me.com>
2015-09-25 22:27:25 -0700

    fixing production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
+  config.action_mailer.default_url_options = { host: 'blocmarks-noel123iamme.herokuapp.com' }
+ end
